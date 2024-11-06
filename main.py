621272ea668e6ece0878b67c3b0dd69e895ad856
EricSanchez <erc.sanchez@gmail.com>
2024-11-05 23:03:25 -0300

    Meu teste

---
+++ main.py
@@ -0,0 +1,20 @@
+print ("jornada de dados")
+
+#variaveis
+idade = 39
+nome = "Eric"
+
+#print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4,5,6]
+
+#metodos
+lista_invertida = lista.reverse()
+
+print(lista_invertida)
